fc1a430202d30cded0378ae2c827aea918d77757
VictorElias2 <victorfalciroli966@gmail.com>
2022-02-05 18:49:26 -0300

    Mudanças simples de design.

--- 1_menu.R
+++ 1_menu.R
@@ -8,22 +8,33 @@ ui <- fluidPage(
   titlePanel("Base de dados de Planetas"),
   sidebarLayout(
     sidebarPanel(
+      width = 3,
       # Adicionar uma imagem de algum planeta no futuro.
-      sliderInput("mass_slider", "Massa", value = min(unique(data$massa)),
-                  min = min(unique(data$massa)),
-                  max = max(unique(data$massa)))
-    ),
+      sliderInput("year_slider", "Ano", value = 2020,
+                  min = min(unique(data$descoberto)),
+                  max = max(unique(data$descoberto))),
+      # Desenvolvimento no futuro
+      # selectInput("select_colum", "Select a Colum (SOON)", 
+      #             choices = c("Selecionar", names(data))),
+      selectInput("select_molecule", "Select a planet with molecule",
+                  choices = c("Selecionar", levels(data$moleculas))),
+    position = "right"),
     mainPanel(
-      dataTableOutput("planetas")
+      tabsetPanel(
+        tabPanel("Tabela",
+                 dataTableOutput("planetas")
+        ),
+        tabPanel("Plot",
+                 plotOutput("plot_planets")
+        )
+      )
     )
   )
 )
 
 server <- function (input, output, session){
-  output$planetas <- renderDataTable({
-    data %>% 
-      filter(massa == input$mass_slider)
-  })
+  source("includes/1_data_planets.R", local = T)
+  source("includes/2_plot_planets.R", local = T)
 }
 
 shinyApp(ui, server)
